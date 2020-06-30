99b5ebc0a4e0bf0e46f5b7f18e01888f540be54d
Uduak Essien <acushla4real@gmail.com>
2020-06-30 14:08:18 +0100

    add game, player and board class with display_board method

--- bin/main.rb
+++ bin/main.rb
@@ -1,2 +1,35 @@
 #!/usr/bin/env ruby
-puts 'Hello World!'
+
+class Game
+    
+end
+
+class Player
+    attr_accessor :name, :piece
+
+    def initialize(name, piece, board)
+        @name = name
+        @piece = piece
+        @board = board
+    end
+end
+
+class Board
+    def initialize
+        @board = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    end
+
+    def display_board
+        puts "#{@board[0]} | #{@board[1]} | #{@board[2]}"  
+        puts "---------"
+        puts "#{@board[3]} | #{@board[4]} | #{@board[5]}"
+        puts "---------"
+        puts "#{@board[6]} | #{@board[7]} | #{@board[8]}"
+    end
+
+
+end
+
+
+new_board = Board.new
+new_board.display_board
