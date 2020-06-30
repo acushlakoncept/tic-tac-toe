ba3fb33ad4fa6ae789dd2442c2e5c0b0e7ee01ff
Uduak Essien <acushla4real@gmail.com>
2020-06-30 15:26:31 +0100

    fix rubocop lint errors

--- bin/main.rb
+++ bin/main.rb
@@ -1,97 +1,90 @@
 #!/usr/bin/env ruby
 
 class Game
-    def initialize
-      @board = Board.new
-      puts "Player 1, input name:"
-      @player_1 = Player.new(gets.chomp, "X", @board)
-      puts "Player 2, input name:"
-      @player_2 = Player.new(gets.chomp, "O", @board)
-      @current_player = @player_1
-    end
+  def initialize
+    @board = Board.new
+    puts 'Player 1, input name:'
+    @player1 = Player.new(gets.chomp, 'X', @board)
+    puts 'Player 2, input name:'
+    @player2 = Player.new(gets.chomp, 'O', @board)
+    @current_player = @player1
+  end
 
-    def play
-      #loop
-      
-      puts "Game has started!"
-      #render the board
-      @board.display_board
-      #ask for the current players position
-      puts "#{@current_player.name}, Choose from 1 - 9"
-      #break out of the loop If the game is over
-      #switch players
-    end
+  def play
+    # loop
 
-    def is_game_over?
-        #check_winner
-        #check_draw
-    end
+    puts 'Game has started!'
+    # render the board
+    @board.display_board
+    # ask for the current players position
+    puts "#{@current_player.name}, Choose from 1 - 9"
+    # break out of the loop If the game is over
+    # switch players
+  end
 
-    def check_winner
-        #check if current player has a winner combination
-        #display win message
-    end
+  def game_over?
+    # check_winner
+    # check_draw
+  end
 
-    def check_draw
-        #check if board is filled up
-        #display draw message
-    end
+  def check_winner
+    # check if current player has a winner combination
+    # display win message
+  end
 
-    def switch_players
-        #player 1 > player 2 or vice versa
-    end
+  def check_draw
+    # check if board is filled up
+    # display draw message
+  end
+
+  def switch_players
+    # player 1 > player 2 or vice versa
+  end
 end
 
 class Player
-    attr_accessor :name, :piece
+  attr_accessor :name, :piece
 
-    def initialize(name, piece, board)
-        @name = name
-        @piece = piece
-        @board = board
-    end
+  def initialize(name, piece, board)
+    @name = name
+    @piece = piece
+    @board = board
+  end
 
-    def ask_position
-        #get user position [1-9]
-        #check if position is valid
-        #set_piece
-        #else
-        #display_error and ask_position again
-    end
-
-    
+  def ask_position
+    # get user position [1-9]
+    # check if position is valid
+    # set_piece
+    # else
+    # display_error and ask_position again
+  end
 end
 
 class Board
-    def initialize
-        @board = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
-    end
+  def initialize
+    @board = %w[1 2 3 4 5 6 7 8 9]
+  end
 
-    def display_board
-        puts "#{@board[0]} | #{@board[1]} | #{@board[2]}"  
-        puts "---------"
-        puts "#{@board[3]} | #{@board[4]} | #{@board[5]}"
-        puts "---------"
-        puts "#{@board[6]} | #{@board[7]} | #{@board[8]}"
-    end
+  def display_board
+    puts "#{@board[0]} | #{@board[1]} | #{@board[2]}"
+    puts '---------'
+    puts "#{@board[3]} | #{@board[4]} | #{@board[5]}"
+    puts '---------'
+    puts "#{@board[6]} | #{@board[7]} | #{@board[8]}"
+  end
 
-    def set_piece
-        #set piece X or O on the board
-    end
+  def set_piece
+    # set piece X or O on the board
+  end
 
-    def win_combo
-        #winning combinations
-    end
+  def win_combo
+    # winning combinations
+  end
 
-    def is_full?
-        #is there still room to place piece?
-    end
-
+  def full?
+    # is there still room to place piece?
+  end
 end
-
-
-# new_board = Board.new
-# new_board.display_board
 
 new_game = Game.new
 new_game.play
