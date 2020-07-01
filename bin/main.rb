066a2c994a088b10663fe8cda227d9888cf4f85d
Uduak Essien <acushla4real@gmail.com>
2020-07-01 09:35:35 +0100

    change gets.chomp to gets.strip to strip out empty spaces

--- bin/main.rb
+++ bin/main.rb
@@ -4,9 +4,9 @@ class Game
   def initialize
     @board = Board.new
     puts 'Player 1, input name:'
-    @player1 = Player.new(gets.chomp, 'X', @board)
+    @player1 = Player.new(gets.strip, 'X', @board)
     puts 'Player 2, input name:'
-    @player2 = Player.new(gets.chomp, 'O', @board)
+    @player2 = Player.new(gets.strip, 'O', @board)
     @current_player = @player1
   end
 
@@ -88,5 +88,24 @@ class Board
   end
 end
 
-new_game = Game.new
-new_game.play
+# new_game = Game.new
+# new_game.play
+
+# arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# p arr.include?(0)
+# p arr
+# p arr.delete(8)
+# p arr
+
+
+# p [1].empty?
+# ar = ["1", "2", "X", "4", "X", "6", "X", "8", "9" ]
+# arry = [[0,4,8], [2,4,6]]
+
+# arry.each do |dia|
+  
+# end
+
+name = gets.strip
+p name
+
