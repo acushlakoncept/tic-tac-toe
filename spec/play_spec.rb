7d267e3eb62aa2a411d1890ec5f43c96fb04d82f
Uduak Essien <acushla4real@gmail.com>
2020-07-09 16:54:39 +0100

    add rspec test for valid_position?

--- spec/play_spec.rb
+++ spec/play_spec.rb
@@ -2,11 +2,18 @@ require_relative '../lib/player.rb'
 require_relative '../lib/board.rb'
 
 describe Player do
-  it 'should receive 3 arguments' do
     board = Board.new
     player = Player.new("Uduak", "X", board)
+  it 'should receive 3 arguments' do
     expect(player.name).to eq("Uduak")
     expect(player.piece).to eq("X")
   end
   
+  describe "#valid_position?" do
+    it 'should return true for any integer between 1-9' do
+      expect(player.valid_position?(2)).to be(true)
+      expect(player.valid_position?(0)).to be(false)
+      expect(player.valid_position?(11)).to be(false)
+    end
+  end
 end
