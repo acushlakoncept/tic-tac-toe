a7dc7caa06758903a342b60efdc2d727e205e3f8
Uduak Essien <acushla4real@gmail.com>
2020-07-10 12:54:58 +0100

    add rspec test case for check_winner?

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,6 +3,20 @@ require_relative '../lib/board.rb'
 require_relative '../lib/player.rb'
 
 describe Game do
-  
+  board = Board.new
+  player1 = Player.new("Uduak", "X", board)
+  player2 = Player.new("Elijah", "O", board)
+  game = Game.new(player1, player2, board)
+  single_proc = proc { |elem| puts elem }
+
+  describe "#check_winner?" do
+    win_proc = proc { |name| puts "Hurray!!! #{name}, You won!" }
+    it 'should return true if there is a winner' do
+      board.board = %w[X X X 4 5 6 7 8 9]
+      expect(game.check_winner?(win_proc, single_proc)).to be(true)
+    end
+  end
+
+
 end
 
