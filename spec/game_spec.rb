4a02e41716a992fe0f961f5fed338282150b270b
Uduak Essien <acushla4real@gmail.com>
2020-07-10 15:00:07 +0100

    refactored game_spec to use let

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,11 +3,11 @@ require_relative '../lib/board.rb'
 require_relative '../lib/player.rb'
 
 describe Game do
-  board = Board.new
-  player1 = Player.new('Uduak', 'X', board)
-  player2 = Player.new('Elijah', 'O', board)
-  game = Game.new(player1, player2, board)
-  test_proc = proc { |elem| }
+  let(:board) {Board.new}
+  let(:player1) {Player.new('Uduak', 'X', board)}
+  let(:player2) {Player.new('Elijah', 'O', board)}
+  let(:game) {Game.new(player1, player2, board)}
+  let(:test_proc) {proc { |elem| }}
 
   describe '#check_winner?' do
     it 'should return true if there is a winner' do
