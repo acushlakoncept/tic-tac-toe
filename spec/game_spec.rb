2d5fd30ba143b1665e4cc89520cb67aed1cb4ecb
Uduak Essien <acushla4real@gmail.com>
2020-07-10 14:55:29 +0100

    removed duplicate proc and retained just 1

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -7,21 +7,19 @@ describe Game do
   player1 = Player.new('Uduak', 'X', board)
   player2 = Player.new('Elijah', 'O', board)
   game = Game.new(player1, player2, board)
-  single_proc = proc { |elem| }
+  test_proc = proc { |elem| }
 
   describe '#check_winner?' do
     it 'should return true if there is a winner' do
-      win_proc = proc { |name|  }
       board.board = %w[X X X 4 5 6 7 8 9]
-      expect(game.check_winner?(win_proc, single_proc)).to be(true)
+      expect(game.check_winner?(test_proc, test_proc)).to be(true)
     end
   end
 
   describe '#check_draw?' do
     it 'should return true if the board is full' do
-      draw_proc = proc {}
       board.positions = []
-      expect(game.check_draw?(draw_proc, single_proc)).to be(true)
+      expect(game.check_draw?(test_proc, test_proc)).to be(true)
     end
   end
 end
