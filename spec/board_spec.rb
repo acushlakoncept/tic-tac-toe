e7051fcd2f08a093f9174c82edcd2a309627f0b1
Elijah <eligenius@gmail.com>
2020-07-10 11:40:15 +0100

    add test for #valid_piece_placement?

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -4,15 +4,22 @@ require_relative '../lib/game.rb'
 describe Board do
   board = Board.new
   game = Game.new("Elijah", "Uduak", board)
-
+  
+  describe "#valid_piece_placement?" do
+    it "should return true if piece placement is valid" do
+      expect(board.valid_piece_placement?(4)).to be(true)
+    end
+  end
+  
   describe "#full?" do
     it 'should return true when full' do
-      # expect(board.positions.count).to eq(9)
       board.positions = []
       expect(board.full?).to be(true)
     end
   end
+
   
+
   # # check_winner?
   # describe "#check_winner?" do
   #   xit 'should return true if game won' do
